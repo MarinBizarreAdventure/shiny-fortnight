61ce6664e90bd15ca3b731c14d5b5bfcd30be372
Negai Marin <negaimarin@gmail.com>
2026-03-08 17:00:43 +0200

    feat: add persistent memory in json files

--- app/repository/task_repository.py
+++ app/repository/task_repository.py
@@ -1,9 +1,31 @@
 from app.models.task import Task
-
+from pydantic import TypeAdapter
+import json
+from pathlib import Path
 class TaskRepository():
-    def __init__(self):
-        self.tasks: list[Task] = []
-        self._id_iterator: int = 0
+    def __init__(self, file_path: str):
+        self.file_path = Path(file_path)
+        self.tasks: list[Task] = self._load()
+        self._id_iterator: int = max((t.id for t in self.tasks), default=0)
+
+    def _load(self) -> list[Task]:
+        if Path(self.file_path).exists():
+            with open(self.file_path, "r") as f:
+                raw_data = json.load(f)
+                adapter = TypeAdapter(list[Task])
+                return adapter.validate_python(raw_data)
+        return []
+        
+
+    def _save(self):
+        self.file_path.parent.mkdir(parents=True, exist_ok=True)
+
+        with open(self.file_path, "w") as f:
+                # data_to_save = TypeAdapter(list[Task]).dump_python(self.tasks)
+                data_to_save = [t.model_dump() for t in self.tasks]
+                json.dump(data_to_save,f,indent = 4)
+    
+
 
     def get_new_id(self) -> int:
         self._id_iterator += 1
@@ -18,6 +40,7 @@ class TaskRepository():
     def create(self, title: str) -> Task:
         new_task = Task(id=self.get_new_id(),title=title)
         self.tasks.append(new_task)
+        self._save()
         return new_task
     
     def delete(self, task_id: int) -> bool:
@@ -25,6 +48,10 @@ class TaskRepository():
         if task is None:
             return False
         self.tasks.remove(task)
+        self._save()
         return True
     
   
+    def update(self, task: Task) -> Task:
+        self._save()
+        return task
